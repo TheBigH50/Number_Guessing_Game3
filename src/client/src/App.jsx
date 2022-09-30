68dee09e9a414bc3be3d241017bae8681710bd6e
TheBigH50 <HFangmeier@outlook.com>
2022-09-30 07:06:47 -0500

    set up current user score link

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,21 +1,44 @@
 import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
 import { HomePage, LeaderBoard } from "./pages";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
+  let [user, setUser] = useState(null);
+  let [isLoading, setIsLoading] = useState(true);
 
+  function getUser() {
+    fetch("http://localhost:5000/api/leaderboard/TheBigH50")
+      .then((res) => res.json())
+      .then((current) => {
+        setUser(current);        
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+   getUser();
+  }, []);
   
-
+  
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Is Loading...</h1>
+      </div>
+    );
+  } else {
   return (
     <BrowserRouter>
+    <h2>Welcome ${user.users.UserName}</h2>
     <nav>
       <ul>
         <li>
           <NavLink to={"/"}>Home</NavLink>
         </li>
         <li>
-          <NavLink to={"/leaderBoard"}>Leader Board</NavLink>
+          <NavLink to={`/leaderBoard/${user.users.UserID}`}>Leader Board</NavLink>
         </li>
       </ul>
     </nav>
@@ -25,6 +48,7 @@ function App() {
     </Routes>
     </BrowserRouter>    
   );
+}
 }
 
 export default App;
--- src/client/src/pages/leaderBoard.page.jsx
+++ src/client/src/pages/leaderBoard.page.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function LeaderBoard(props) {
 let [isLoading, setIsLoading] = useState(true);
 let [easyList, setEasyList] = useState(null);
 let [hardList, setHardList] = useState(null);
 let [overkillList, setOverkillList] = useState(null);
+let [currentScore, setCurrentScore] = useState(null);
+let { UserID } = useParams();
 
 function getEasy() {
   fetch("http://localhost:5000/api/leaderboard/easy")
@@ -28,7 +31,16 @@ function getOverkill() {
   fetch("http://localhost:5000/api/leaderboard/overkill")
     .then((res) => res.json())
     .then((overkill) => {
-      setOverkillList(overkill);
+      setOverkillList(overkill);         
+    })
+    .catch((error) => console.error(error));
+}
+
+function getCurrentScore() {
+  fetch(`http://localhost:5000/api/leaderboard/${UserID}`)
+    .then((res) => res.json())
+    .then((current) => {
+      setCurrentScore(current);
       setIsLoading(false);    
     })
     .catch((error) => console.error(error));
@@ -38,11 +50,13 @@ useEffect(() => {
   getEasy();
   getHard();
   getOverkill();
+  getCurrentScore();
 }, []);
 
 console.log(easyList);
 console.log(hardList);
 console.log(overkillList);
+console.log(currentScore);
 if (isLoading) {
     return (
       <div>
