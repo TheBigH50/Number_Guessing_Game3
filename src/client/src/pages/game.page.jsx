f055da8e33c87408554c7b4d058bd3503ce2c440
TheBigH50 <HFangmeier@outlook.com>
2022-10-05 19:23:14 -0500

    start game page

---
+++ src/client/src/pages/game.page.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Game() {
+
+
+
+    
+    
+    
+    if (isLoading) {
+        return (
+          <div>
+            <h1>Is Loading...</h1>
+          </div>
+        );
+      } else {
+        return (
+
+            <h1>Hello World!</h1>
+
+
+        );
+}
+}
