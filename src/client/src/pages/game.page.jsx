d3f1f6d08035278311b187b0f2a7ba369cea85b1
TheBigH50 <HFangmeier@outlook.com>
2022-10-05 19:50:45 -0500

    set up easy random api fetch

--- src/client/src/pages/game.page.jsx
+++ src/client/src/pages/game.page.jsx
@@ -3,9 +3,31 @@ import { useParams } from "react-router-dom";
 
 function Game() {
 
+let [ easyGame, setEasyGame ] = useState([]);
+let [ isLoading, setIsLoading ] = useState(true);
 
-
-    
+    function gameEasy() {
+        fetch("https://api.random.org/json-rpc/4/invoke", {
+            "jsonrpc": "2.0",
+            "method": "generateIntegers",
+            "params": {
+                "apiKey": "86555fa2-c0e1-4380-83f7-e862fdd2d686",
+                "n": 1,
+                "min": 1,
+                "max": 10,
+                "replacement": false,
+                "base": 10,
+                "pregeneratedRandomization": null
+            },
+            "id": 23412
+        })
+          .then((res) => res.json())
+          .then((easy) => {
+            setEasyGame(easy);
+            setIsLoading(false);
+          })
+          .catch((error) => console.error(error));
+      }
     
     
     if (isLoading) {
@@ -16,9 +38,11 @@ function Game() {
         );
       } else {
         return (
-
+<div>
             <h1>Hello World!</h1>
-
+            <p>{`My number is: ${easyGame.data}`}</p>
+            </div>
+            
 
         );
 }
