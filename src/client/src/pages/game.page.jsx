1f1baf5378872d04611586433bb19e40a5f0339b
TheBigH50 <HFangmeier@outlook.com>
2022-10-11 13:55:04 -0500

    fix fetch

--- src/client/src/pages/game.page.jsx
+++ src/client/src/pages/game.page.jsx
@@ -7,20 +7,33 @@ let [ easyGame, setEasyGame ] = useState([]);
 let [ isLoading, setIsLoading ] = useState(true);
 
     function gameEasy() {
-        fetch("https://api.random.org/json-rpc/4/invoke", {
-            "jsonrpc": "2.0",
-            "method": "generateIntegers",                      
-            "params": {
-                "apiKey": "86555fa2-c0e1-4380-83f7-e862fdd2d686",
-                "n": 1,
-                "min": 1,
-                "max": 10,
-                "replacement": false,
-                "base": 10,
-                "pregeneratedRandomization": null
-            },
-            "id": 23412
-        })
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Cookie", "__cflb=02DiuEMAZFhhWAbaKrDP2YYpUK2thcATmzfaiAhCEFueK");
+        
+        var raw = JSON.stringify({
+          "jsonrpc": "2.0",
+          "method": "generateIntegers",
+          "params": {
+            "apiKey": "86555fa2-c0e1-4380-83f7-e862fdd2d686",
+            "n": 1,
+            "min": 1,
+            "max": 10,
+            "replacement": false,
+            "base": 10,
+            "pregeneratedRandomization": null
+          },
+          "id": 23412
+        });
+        
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
+        
+        fetch("https://api.random.org/json-rpc/4/invoke", requestOptions)          
           .then((res) => res.json())
           .then((easy) => {
             setEasyGame(easy);
